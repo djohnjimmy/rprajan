abc9c926acad2126f89fe4a30d4477c3df089fca
John Dondapati <djohnjimmy@gmail.com>
2015-07-24 11:31:50 +0530

    removing filter on gain

--- zfproject/module/Stocks/src/Stocks/Model/StocksTable.php
+++ zfproject/module/Stocks/src/Stocks/Model/StocksTable.php
@@ -24,7 +24,7 @@ class StocksTable
             $yesterday = date ( 'Y-m-d ', mktime ( 0, 0, 0, date ( "m" ), date ( "d" )-1, date ( "Y" ) ) );
             $today = date ( 'Y-m-d ', mktime ( 0, 0, 0, date ( "m" ), date ( "d" ), date ( "Y" ) ));
             $select->join(array('p' => 'scrip'), 'p.id = stocks.scrip_id');
-            $select->where->greaterThan('gain', 6);
+//             $select->where->greaterThan('gain', 6);
             $select->where->between('timestamp', $yesterday, $today);
 //             $select->order('gain DSC')->limit(2);
             $select->order('gain DESC');
